4fb33bf7c7c9ca834a15080b922ccb1872af446d
yuvhrajVirSinghVerma <vermayuvhraj@gmail.com>
2024-12-30 08:32:43 +0530

    centralised config server

---
+++ ConfigServer/src/test/java/com/services_configs/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services_configs.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
